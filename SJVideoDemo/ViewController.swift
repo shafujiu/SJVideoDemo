169448329003a6a377fddb0ffc87fd66eb384328
赵维 <zhaowei200711@163.com>
2020-12-15 09:41:47 +0800

    Initial Commit

---
+++ SJVideoDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SJVideoDemo
+//
+//  Created by Shafujiu on 2020/12/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
